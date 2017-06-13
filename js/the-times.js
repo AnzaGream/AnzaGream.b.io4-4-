350704eb40149034d4ff68704690669d2c1633d6
AnzaGream <35warface@mail.ru>
2017-06-13 14:44:55 +0700

    Update the-times.js

--- js/the-times.js
+++ js/the-times.js
@@ -266,7 +266,7 @@ $(function() {
 	
 	// 16. YTPlayer
     $("#background-video").YTPlayer({
-        videoId: "KYMLQGtznTo", // DEMO URL is: https://www.youtube.com/watch?v=ga-LSXrrWNI
+        videoId: "BsUFqI1AgzI", // DEMO URL is: https://www.youtube.com/watch?v=ga-LSXrrWNI
         mute: false,             // options: true, false
         pauseOnScroll: false,
         repeat: true,
