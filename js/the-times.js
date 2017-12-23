b38001d8d16ff315f2944f3735ca8c0786b298e0
AnzaGream <35warface@mail.ru>
2017-12-23 21:21:27 +0700

    Update the-times.js

--- js/the-times.js
+++ js/the-times.js
@@ -266,7 +266,7 @@ $(function() {
 	
 	// 16. YTPlayer
     $("#background-video").YTPlayer({
-        videoId: "CW5oGRx9CLM", // DEMO URL is: https://www.youtube.com/watch?v=ga-LSXrrWNI
+        videoId: "yY7iGa4t9-I", // DEMO URL is: https://www.youtube.com/watch?v=ga-LSXrrWNI
         mute: false,             // options: true, false
         pauseOnScroll: false,
         repeat: true,
